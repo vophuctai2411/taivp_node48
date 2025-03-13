57927104215a4ef175143555369953809fd9bd0c
tai-metacrew <136056530+tai-metacrew@users.noreply.github.com>
2025-03-14 00:12:47 +0700

    update

--- youtube_BE/src/config/jwt.js
+++ youtube_BE/src/config/jwt.js
@@ -3,11 +3,36 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const createAccessToken = (payload) =>{
-    return jwt.sign({payload}, process.env.SECRET_KEY, {
-        algorithm: "HS256",
-        expiresIn: "2h"
-    });
-}
+const createAccessToken = (payload) => {
+  return jwt.sign({ payload }, process.env.SECRET_KEY, {
+    algorithm: "HS256",
+    expiresIn: "2h",
+  });
+};
 
-export { createAccessToken }
+const verifyAccessToken = (accessToken) => {
+  try {
+    const payload = jwt.verify(accessToken, process.env.SECRET_KEY);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+const middlewareToken = (req, res, next) => {
+  let { token } = req.headers;
+  //truong hop 1: khong co token
+  if (!token) {
+    return res.status(401).json({ message: "Authorized" });
+  }
+  //truong hop 2: token khong hop le
+  let checkToken = verifyAccessToken(token);
+  if (checkToken) {
+    return res.status(401).json({ message: "Authorized" });
+  }
+
+  //TH3: token hop le
+  next();
+};
+
+export { createAccessToken, verifyAccessToken, middlewareToken };
--- youtube_BE/src/routes/authRoutes.js
+++ youtube_BE/src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {register, login} from "../controllers/authController.js"
+import { register, login } from "../controllers/authController.js";
+import { middlewareToken } from "../config/jwt.js";
 
 const authRoutes = express.Router();
 
 authRoutes.post("/register", register);
-authRoutes.post("/login", login)
+authRoutes.post("/login", middlewareToken, login);
 
 export default authRoutes;
